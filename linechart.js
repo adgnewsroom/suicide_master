dc6321c4fc7f1eafc880b067ef2d6d7313e9d91d
Yutao Chen <yutaochen95@gmail.com>
2020-02-14 11:12:42 -0600

    update

---
+++ linechart.js
@@ -0,0 +1,322 @@
+
+      var glines
+      var mouseG
+      var tooltip2
+
+      var parseDate = d3.timeParse("%Y")
+
+      var margin = {top: 50, right: 50, bottom: 1, left: 50}
+      var width = 1600 - margin.left - margin.right
+      var height = 800 - margin.top - margin.bottom
+
+      var lineOpacity = 1
+      var lineStroke = "4px"
+
+      var axisPad = 7 // axis formatting
+      var R = 6 //legend marker
+
+      var category = ["Arkansas", "Texas", "Tennessee", "Oklahoma", "Missouri", "Mississippi", "Louisiana"]
+
+      var color = d3.scaleOrdinal()
+        .domain(category)
+        .range(["#a50f15", "#80b1d3", "#fdb462", "#e78ac3", "#9e9ac8", "#5ab220", "#ffffb3"])
+      d3.csv("data.csv", data => {
+
+        var res = data.map((d,i) => {
+          return {
+            date : parseDate(d.year),
+            bidding_no : d.bidding_no,
+            state : d.state,
+            rate : d.rate
+          }
+        })
+
+        var xScale = d3.scaleTime()
+          .domain(d3.extent(res, d=>d.date))
+          .range([0, width])
+
+
+        var yScale = d3.scaleLinear()
+          .domain([0, 21])
+          .range([height, 0]);
+
+        var svg = d3.select("#chart").append("svg")
+          .attr("width", "100%")
+          .attr("height", "100%")
+          .attr("viewBox", "20 20 " + 1600 + " " + 980)
+          .append('g')
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+        // CREATE AXES //
+        // render axis first before lines so that lines will overlay the horizontal ticks
+        var xAxis = d3.axisBottom(xScale).ticks(d3.timeYear.every(1)).tickSizeOuter(axisPad*2).tickSizeInner(axisPad*2)
+        var yAxis = d3.axisLeft(yScale).ticks(10, "s").tickSize(-width) //horizontal ticks across svg width
+
+        svg.append("g")
+          .attr("class", "x axis")
+          .attr("transform", `translate(0, ${height})`)
+          .call(xAxis)
+          .call(g => {
+            var years = xScale.ticks(d3.timeYear.every(1))
+            var xshift = (width/(years.length))/5 - 16
+            g.selectAll("text").attr("transform", `translate(${xshift}, 0)`) //shift tick labels to middle of interval
+              .style("text-anchor", "left")
+              .attr("y", axisPad+10)
+              .attr('fill', 'black')
+              .style("font-family", "Roboto")
+              .style("font-size", 15)
+              .style("font-weight", "bold")
+
+            g.selectAll("line")
+              .attr('stroke', 'black')
+
+            g.select(".domain")
+              .attr('stroke', 'black')
+
+          })
+
+
+
+        svg.append("g")
+          .attr("class", "y axis")
+          .call(yAxis)
+          .call(g => {
+            g.selectAll("text")
+            .style("text-anchor", "middle")
+            .attr("x", -axisPad*2)
+            .attr('fill', 'black')
+            .style("font-family", "Roboto")
+            .style("font-size", 15)
+
+
+
+            g.selectAll("line")
+              .attr('stroke', 'black')
+              .attr('stroke-width', 0.7) // make horizontal tick thinner and lighter so that line paths can stand out
+              .attr('opacity', 0.3)
+
+            g.select(".domain").remove()
+
+           })
+          .append('text')
+            .attr('x', 130)
+            .attr("y", 5)
+            .attr("fill", "black")
+            .text("Suicides per 100,000")
+            .style("font-family", "Roboto")
+            .style("font-size", "15px")
+
+
+        // CREATE LEGEND //
+        var svgLegend = svg.append('g')
+            .attr('class', 'gLegend')
+            .attr("transform", "translate(" + (width + 20) + "," + 0 + ")")
+
+        var legend = svgLegend.selectAll('.legend')
+          .data(category)
+          .enter().append('g')
+            .attr("class", "legend")
+            .attr("transform", function (d, i) {return "translate(0," + i * 25 + ")"})
+
+    svgLegend.append("rect")
+            .attr("width", 115)
+            .attr("height", 185)
+            .attr("x", -120)
+            .attr("y", 490)
+            .style("fill", "black")
+            .style("opacity", "0.1")
+
+
+        legend.append("circle")
+            .attr("class", "legend-node")
+            .attr("cx", -101)
+            .attr("cy", 508)
+            .attr("r", 8)
+            .style("fill", d=>color(d))
+            // .style("padding", "30px")
+
+        legend.append("text")
+            .attr("class", "legend-text")
+            .attr("x", -90)
+            .attr("y", 514)
+            .style("fill", "black")
+            .style("font-size", 15)
+            .style("font-family", "Roboto")
+            .text(d=>d)
+
+
+        // line generator
+        var line = d3.line()
+          .x(d => xScale(d.date))
+          .y(d => yScale(d.rate))
+
+        renderChart(1) // inital chart render (set default to Bidding Exercise 1 data)
+
+        function renderChart(bidding_no) {
+        //
+          var resNew = res.filter(d=>d.bidding_no == parseInt(bidding_no))
+        //
+
+          var res_nested = d3.nest() // necessary to nest data so that keys represent each state category
+            .key(d=>d.state)
+            .entries(resNew)
+
+          // APPEND MULTIPLE LINES //
+          var lines = svg.append('g')
+            .attr('class', 'lines')
+
+          glines = lines.selectAll('.line-group')
+            .data(res_nested).enter()
+            .append('g')
+            .attr('class', 'line-group')
+
+          glines
+            .append('path')
+              .attr('class', 'line')
+              .attr('d', d => line(d.values))
+              .style('stroke', (d, i) => color(i))
+              .style('fill', 'none')
+              .style('opacity', lineOpacity)
+              .style('stroke-width', lineStroke)
+
+
+          // APPEND CIRCLE MARKERS //
+          var gcircle = lines.selectAll("circle-group")
+            .data(res_nested).enter()
+            .append("g")
+            .attr('class', 'circle-group')
+
+          gcircle.selectAll("circle")
+            .data(d => d.values).enter()
+            .append("g")
+            .attr("class", "circle")
+            .append("circle")
+            .attr("cx", d => xScale(d.date))
+            .attr("cy", d => yScale(d.rate))
+            .attr("r", 2)
+
+          // CREATE HOVER TOOLTIP WITH VERTICAL LINE //
+          tooltip2 = d3.select("#chart").append("div")
+            .attr('id', 'tooltip')
+            .style('position', 'absolute')
+            .style("background-color", "Azure")
+            .style('padding', 4)
+            .style('display', 'none')
+
+          mouseG = svg.append("g")
+            .attr("class", "mouse-over-effects");
+
+          mouseG.append("path") // create vertical line to follow mouse
+            .attr("class", "mouse-line")
+            .style("stroke", "#636363")
+            .style("stroke-width", 1.5)
+            .style("opacity", "0");
+
+          var lines = document.getElementsByClassName('line');
+
+          var mousePerLine = mouseG.selectAll('.mouse-per-line')
+            .data(res_nested)
+            .enter()
+            .append("g")
+            .attr("class", "mouse-per-line");
+
+          mousePerLine.append("circle")
+            .attr("r", 3)
+            .style("stroke", function (d) {
+              return color(d.key)
+            })
+            .style("fill", "none")
+            .style("stroke-width", 2)
+            .style("opacity", "0");
+
+          mouseG.append('svg:rect') // append a rect to catch mouse movements on canvas
+            .attr('width', width)
+            .attr('height', height)
+            .attr('fill', 'none')
+            .attr('pointer-events', 'all')
+            .on('mouseout', function () { // on mouse out hide line, circles and text
+              d3.select(".mouse-line")
+                .style("opacity", "0");
+              d3.selectAll(".mouse-per-line circle")
+                .style("opacity", "0");
+              d3.selectAll(".mouse-per-line text")
+                .style("opacity", "0");
+              d3.selectAll("#tooltip")
+                .style('display', 'none')
+
+            })
+            .on('mouseover', function () { // on mouse in show line, circles and text
+              d3.select(".mouse-line")
+                .style("opacity", "1");
+              d3.selectAll(".mouse-per-line circle")
+                .style("opacity", "1");
+              d3.selectAll("#tooltip")
+                .style('display', 'block')
+            })
+            .on('mousemove', function () { // update tooltip content, line, circles and text when mouse moves
+              var mouse = d3.mouse(this)
+
+              d3.selectAll(".mouse-per-line")
+                .attr("transform", function (d, i) {
+                  var xDate = xScale.invert(mouse[0]) // use 'invert' to get date corresponding to distance from mouse position relative to svg
+                  var bisect = d3.bisector(function (d) { return d.date; }).left // retrieve row index of date on parsed csv
+                  var idx = bisect(d.values, xDate);
+
+                  d3.select(".mouse-line")
+                    .attr("d", function () {
+                      var data = "M" + xScale(d.values[idx].date) + "," + (height);
+                      data += " " + xScale(d.values[idx].date) + "," + 0;
+                      return data;
+                    });
+                  return "translate(" + xScale(d.values[idx].date) + "," + yScale(d.values[idx].rate) + ")";
+
+                });
+
+              updateTooltipContent(mouse, res_nested)
+
+            })
+          }
+
+      function updateTooltipContent(mouse, res_nested) {
+
+        sortingObj = []
+        res_nested.map(d => {
+          var xDate = xScale.invert(mouse[0])
+          var bisect = d3.bisector(function (d) { return d.date; }).left
+          var idx = bisect(d.values, xDate)
+          sortingObj.push({key: d.values[idx].state, rate: d.values[idx].rate, year: d.values[idx].date.getFullYear()})
+        })
+
+        sortingObj.sort(function(x, y){
+           return d3.descending(x.rate, y.rate);
+        })
+
+        var sortingArr = sortingObj.map(d=> d.key)
+
+        var res_nested1 = res_nested.slice().sort(function(a, b){
+          return sortingArr.indexOf(a.key) - sortingArr.indexOf(b.key) // rank vehicle category based on price of rate
+        })
+
+        tooltip2.html(sortingObj[0].date)
+          .style('display', 'block')
+          .style('left', d3.event.pageX - 110)
+          .style('top', d3.event.pageY + 20)
+          .style('opacity', 0.8)
+          .selectAll()
+          .data(res_nested1).enter() // for each vehicle category, list out name and price of rate
+          .append('div')
+          .attr("class", "tooltiptext")
+          .style('color', "black")
+          .style('font-size', 14)
+          // .style('font-weight', "bold")
+          .style("font-family", "Roboto")
+          .html(d => {
+            var xDate = xScale.invert(mouse[0])
+            var bisect = d3.bisector(function (d) { return d.date; }).left
+            var idx = bisect(d.values, xDate)
+            return d.key + ": " + d.values[idx].rate.toString()
+          })
+      }
+
+    })
--- map.js
+++ map.js
@@ -1,6 +1,8 @@
 var width = 900;
     height = 700;
 
+var currentYear = 1999;
+
 var tip = d3.tip()
   .attr('class', 'd3-tip')
   .offset([-5, 0])
@@ -39,6 +41,12 @@ queue()
     .defer(d3.json, "USA.json")
     .defer(d3.csv, "rate.csv", typeAndSet) // process
     .await(loaded);
+
+
+    Promise.all([
+      d3.json("USA.json"),
+      d3.csv("rate.csv")
+])
 
 function typeAndSet(d) {
     d.rate = +d.rate;
@@ -99,3 +107,208 @@ function loaded(error, usa, rate) {
       .call(legendLinear);
 
 }
+
+
+
+
+//
+//
+//
+//
+//
+//
+
+//DATE PICKER
+var datepicker = svg.append("g")
+                  .attr("transform","translate(480,680)");
+
+datepicker.append("rect")
+  .attr("x", 0)
+  .attr("y", 0)
+  .attr("rx", 10)
+  .attr("ry", 10)
+  .attr("width", 60)
+  .attr("height", 30)
+  .style("fill", "gray");
+
+datepicker.append("text")
+    .attr("id", "yearText")
+    .attr("x", 8)
+    .attr("y", 7)
+    .attr("text-anchor", "start")
+    .attr("alignment-baseline", "hanging")
+    .style("font-size", "20px")
+    .style("padding", "100px")
+    .style("fill", "white")
+    .style("font-family", "'Roboto', sans-serif")
+    .html(currentYear);
+
+    //left arrow
+    var leftarrow = datepicker.append("g")
+       .attr("transform","translate(-30,3)");
+
+    leftarrow.append("rect")
+        .attr("x", 3)
+        .attr("y", 0)
+        .attr("width", 20)
+        .attr("height", 20)
+        .attr("fill", "#cccccc")
+        .style("cursor", "pointer")
+        .on("click", function(){
+
+          //make right reappear if it's invisible
+
+          d3.select("#rightArrow")
+            .style("display", "block");
+
+          if(currentYear > 1999){
+            currentYear -= 1;
+          }
+
+          if(currentYear == 1999) {
+            d3.select("#leftArrow")
+              .style("display", "none")
+              .style("cursor", "default");
+          }
+
+          d3.select("#yearText")
+            .html(currentYear);
+
+          updateBubbleMap(currentYear);
+        });
+
+    leftarrow.append("path")
+       .attr("d", "M12 0c6.623 0 12 5.377 12 12s-5.377 12-12 12-12-5.377-12-12 5.377-12 12-12zm0 1c6.071 0 11 4.929 11 11s-4.929 11-11 11-11-4.929-11-11 4.929-11 11-11zm3 5.753l-6.44 5.247 6.44 5.263-.678.737-7.322-6 7.335-6 .665.753z")
+       .attr("id", "leftArrow")
+       .attr("fill", "gray")
+       .attr("fill-rule", "evenodd")
+       .attr("clip-rule", "evenodd")
+       .style("display", "none")
+       .style("cursor", "pointer")
+       .on("click", function(){
+
+         //make right reappear if it's invisible
+
+         d3.select("#rightArrow")
+           .style("display", "block");
+
+         if(currentYear > 1999){
+           currentYear -= 1;
+         }
+
+         if(currentYear == 1999) {
+           d3.select("#leftArrow")
+             .style("display", "none")
+             .style("cursor", "default");
+         }
+
+         d3.select("#yearText")
+           .html(currentYear);
+
+         updateBubbleMap(currentYear);
+       });
+
+
+
+  //right arrow
+
+  var rightarrow = datepicker.append("g")
+     .attr("transform","translate(65,3)");
+
+  rightarrow.append("rect")
+      .attr("x", 3)
+      .attr("y", 0)
+      .attr("width", 20)
+      .attr("height", 20)
+      .attr("fill", "#cccccc")
+      .style("cursor", "pointer")
+      .on("click", function(){
+
+        d3.select("#leftArrow")
+          .style("display", "block");
+
+          if(currentYear < 2017){
+              currentYear += 1;
+                }
+
+          if(currentYear == 2017) {
+            d3.select("#rightArrow")
+            .style("display", "none")
+            .style("cursor", "default");
+                }
+
+          d3.select("#yearText")
+            .html(currentYear);
+
+          updateBubbleMap(currentYear);
+
+            });
+
+   rightarrow.append("path")
+      .attr("id", "rightArrow")
+      .attr("d", "M12 0c6.623 0 12 5.377 12 12s-5.377 12-12 12-12-5.377-12-12 5.377-12 12-12zm0 1c6.071 0 11 4.929 11 11s-4.929 11-11 11-11-4.929-11-11 4.929-11 11-11zm-3 5.753l6.44 5.247-6.44 5.263.678.737 7.322-6-7.335-6-.665.753z")
+      .attr("fill", "gray")
+      .attr("fill-rule", "evenodd")
+      .attr("clip-rule", "evenodd")
+      .style("cursor","pointer")
+      .on("click", function(){
+          d3.select("#leftArrow")
+            .style("display", "block");
+          if(currentYear < 2017){
+            currentYear += 1;
+          }
+          if(currentYear == 2017) {
+            d3.select("#rightArrow")
+              .style("display", "none")
+              .style("cursor", "default");
+          }
+          d3.select("#yearText")
+            .html(currentYear);
+          updateBubbleMap(currentYear);
+      });
+
+
+
+      var updateBubbleMap = function(newYear){
+
+
+        console.log(data[1].filter(function(d){ return d["CAL_YR"] == newYear; }));
+
+        var bubbles = svg.selectAll(".bubbles")
+          .data(data[1].filter(function(d){ return d["CAL_YR"] == newYear; }).filter(function(d){ return d["LATITUDE"] != 0;}))
+          .enter()
+          .append("circle")
+          .attr("class", "bubbles")
+          .attr("cx", function (d) {
+            if(projectLatLong([d["LONGITUDE"], d["LATITUDE"]]) != null){
+              return projectLatLong([d["LONGITUDE"], d["LATITUDE"]])[0];
+            }
+          })
+          .attr("cy", function (d) {
+            if(projectLatLong([d["LONGITUDE"], d["LATITUDE"]]) != null){
+              return projectLatLong([d["LONGITUDE"], d["LATITUDE"]])[1];
+            }
+          })
+          .attr("r", function(d){ return accidentScale(d["accident_rate"]); })
+          .attr("fill", "#de2d26")
+          .attr("opacity", 0.5)
+          .attr("stroke", "white")
+          .attr("stroke-width", 0.5)
+          .on("mouseover", function(d) {
+                d3.select(".tooltip")
+                  .style("opacity", 1)
+                  .style("left", (d3.event.pageX) + "px")
+                  .style("top", (d3.event.pageY-5) + "px")
+                  .style("font-size", "12px")
+                  .style("font-family", "'Roboto', sans-serif")
+                  .html("<strong style='color:firebrick; font-size:11pt;'>" + d["CURRENT_MINE_NAME"] +
+                  "</strong>" + "<br>" + "<br>" + "Accident rate: " + "<strong style='color:firebrick;'>" +
+                  d["accident_rate"] + "</strong>" + "<br>" + "Owned by " + "<strong style='color:steelblue;'>" +
+                  d["CONTROLLER_NAME"] + "</strong>" + "<br>" + "Located near " + "<strong style='color:steelblue;'>" +
+                  d["NEAREST_TOWN"] + ", " + d["STATE"]) + "</strong>";
+          })
+          .on("mouseout", function(d) {
+                d3.select(".tooltip")
+                  .style("opacity",0);
+          })
+      }
